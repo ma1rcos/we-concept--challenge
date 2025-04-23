88c355539ff58dc76024256de8cdd9c62e0ad583
ma1rcos <mfbdcarvalho@gmail.com>
2025-04-23 11:49:33 -0300

    first commit

---
+++ src/main/kotlin/br/com/weconcept/challenge/ChallengeApplication.kt
@@ -0,0 +1,11 @@
+package br.com.weconcept.challenge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChallengeApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChallengeApplication>(*args)
+}
