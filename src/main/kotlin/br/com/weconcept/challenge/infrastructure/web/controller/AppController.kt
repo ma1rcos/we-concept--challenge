478b1ac3e6952a8dfa334dc845ef5d021329b656
ma1rcos <mfbdcarvalho@gmail.com>
2025-04-24 12:48:27 -0300

    adding app controller

---
+++ src/main/kotlin/br/com/weconcept/challenge/infrastructure/web/controller/AppController.kt
@@ -0,0 +1,17 @@
+package br.com.weconcept.challenge.infrastructure.web.controller
+
+import br.com.weconcept.challenge.service.AppService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("")
+class AppController(
+    private val appService: AppService
+) {
+
+    @GetMapping("")
+    fun getHello(): String = this.appService.getHello()
+
+}
