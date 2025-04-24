87d17f456f0ff4c21d964ce9ebac8bd16e312c63
ma1rcos <mfbdcarvalho@gmail.com>
2025-04-24 17:19:26 -0300

    updating import app service and updating paths

--- src/main/kotlin/br/com/weconcept/challenge/infrastructure/web/controller/AppController.kt
+++ src/main/kotlin/br/com/weconcept/challenge/infrastructure/web/controller/AppController.kt
@@ -1,17 +1,17 @@
 package br.com.weconcept.challenge.infrastructure.web.controller
 
-import br.com.weconcept.challenge.service.AppService
+import br.com.weconcept.challenge.application.service.AppService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("")
+@RequestMapping
 class AppController(
     private val appService: AppService
 ) {
 
-    @GetMapping("")
+    @GetMapping
     fun getHello(): String = this.appService.getHello()
 
 }
